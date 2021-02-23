8ed96c86613885b202efd32d9a77a37c8df90bbb
Leban Issa <lebanissa3@gmail.com>
2021-02-23 11:54:49 -0500

    added all the table and restaurant endpoints

---
+++ src/main/java/com/waitlist/information/system/waitlist/repository/RestaurantRepository.java
@@ -0,0 +1,8 @@
+package com.waitlist.information.system.waitlist.repository;
+
+import com.waitlist.information.system.waitlist.model.Restaurant;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface RestaurantRepository extends MongoRepository<Restaurant, Integer> {
+
+}
