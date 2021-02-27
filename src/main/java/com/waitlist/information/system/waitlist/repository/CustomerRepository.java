755347d9a2c75ea478e8c7df0dea93185463aa95
Leban Issa <lebanissa3@gmail.com>
2021-02-27 15:37:02 -0500

    added count number of customers endpoint and added some more parameters to restaurant and customer model

--- src/main/java/com/waitlist/information/system/waitlist/repository/CustomerRepository.java
+++ src/main/java/com/waitlist/information/system/waitlist/repository/CustomerRepository.java
@@ -4,9 +4,5 @@ import com.waitlist.information.system.waitlist.model.Customer;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface CustomerRepository extends MongoRepository<Customer, Integer> {
-//    //Customer findById(String id);
-//    Customer findByCustomerName(String name);
-//    Customer findByEmail(String email);
-//    Customer findByNumber(String number);
 
 }
